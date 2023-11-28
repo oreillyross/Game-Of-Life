b4ba13bd166cd6ef940cde9b28626e55900b90e1
oreillyross <oreillyross@gmail.com>
2023-11-28 15:39:39 +0000

    update as_string method

--- pylife/grid.py
+++ pylife/grid.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
 
+ALIVE = "❤️"
+DEAD = "."
+
 class LifeGrid:
   def __init__(self, pattern) -> None:
    self.pattern = pattern
@@ -17,25 +20,31 @@ class LifeGrid:
     
     )
     num_neighbors = defaultdict(int)
-    for row, cols in self.pattern.alive_cells:
-      for drow, dcols in neighbours:
-        num_neighbors = [(row + drow, cols + dcols)] += 1
+    for row, col in self.pattern.alive_cells:
+      for drow, dcol in neighbours:
+        num_neighbors[(row + drow, col + dcol)] += 1
     stay_alive = {
       cell for cell, num in num_neighbors.items() if num in {2,3}
     } & self.pattern.alive_cells
     come_alive = {
       cell for cell, num in num_neighbors.items() if num == 3
-    } - self.pattern.alive_cells
-
-  self.pattern.alive_cells = stay_alive | come_alive
-  
+    } - self.pattern.alive_cells self.pattern.alive_cells = stay_alive | come_alive
     
 
   def as_string(self, bbox):
-    pass
+    start_row, start_col, end_row, end_col = bbox
+    display = [self.pattern.name.center(2 * (end_col - start_col))]
+    for row in range(start_row, end_row):
+      display_row = [
+        ALIVE if (row, col) in self.pattern.alive_cells else DEAD
+        for col in range(start_col, end_col):
+        
+      ]
+      display.append(" ".join(display_row))
+    return "\n".join(display)
 
   def __str__(self):
     return (
       f"{self.pattern.name}\n"
-      f"Alive cells -> {sorted(self.pattern.aliv_cells)}"
+      f"Alive cells -> {sorted(self.pattern.alive_cells)}"
     )
